01f8d87653d8807cdc2cfec145518bc41247c190
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-14 01:15:30 +0000

    initial template site

---
+++ components/Welcome/Welcome.tsx
@@ -0,0 +1,18 @@
+import { Text, Title } from '@mantine/core';
+import classes from './Welcome.module.css';
+
+export function Welcome() {
+  return (
+    <>
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome to the{' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'orange', to: 'yellow' }}>
+          Race Wedding
+        </Text>
+      </Title>
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        We&apos;re excited to say that a fancy wedding website is coming soon! Stay tuned for updates.
+      </Text>
+    </>
+  );
+}
