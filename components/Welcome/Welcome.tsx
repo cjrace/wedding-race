5c2b2cafe51e6e8fd0b94e5030597ef0a50da83d
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-15 23:58:50 +0000

    add prettier

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,13 +5,19 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to the{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'orange', to: 'yellow' }}>
+        Welcome to the{" "}
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: "orange", to: "yellow" }}
+        >
           Race Wedding
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        We&apos;re excited to say that a fancy wedding website is coming soon! Stay tuned for updates.
+        We&apos;re excited to say that a fancy wedding website is coming soon!
+        Stay tuned for updates.
       </Text>
     </>
   );
