29534a1a650a1c360192bf638aabbcfe07c8b345
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-16 00:28:31 +0000

    cleanup

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,5 +1,5 @@
-import { Text, Title } from '@mantine/core';
-import classes from './Welcome.module.css';
+import { Text, Title } from "@mantine/core";
+import classes from "./Welcome.module.css";
 
 export function Welcome() {
   return (
