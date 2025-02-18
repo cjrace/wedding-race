29a9c1b610989a89296b72c96a8296b169fa1b51
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-18 01:32:35 +0000

    have a countdown until christmas... env variable approach is giving some pain so far

--- src/components/DaysToGo/DaysToGo.tsx
+++ src/components/DaysToGo/DaysToGo.tsx
@@ -1,47 +1,49 @@
 "use client";
 
 import { Text } from "@mantine/core";
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState } from "react";
 
-export default function DaysToGo() {
-  const weddingDate = useMemo(() => {
-    const date = new Date(process.env.WEDDING_DATE || "");
-    date.setUTCHours(12, 0, 0, 0); // Set the wedding time to 1pm UK time (which is 12pm UTC in June)
-    console.log(date);
-    return date;
-  }, []);
-  const calculateTimeLeft = useCallback(() => {
-    const now = new Date();
-    const difference = weddingDate.getTime() - now.getTime();
+const DaysToGo = () => {
+  const weddingDateString = process.env.NEXT_PUBLIC_WEDDING_DATE || "2026-12-25T12:00:00";
+  const weddingDate = new Date(weddingDateString);
+  console.log("Wedding Date from env:", weddingDateString);
+  console.log("Parsed Wedding Date:", weddingDate);
 
-    let timeLeft;
-
-    timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
-    };
-
-    return timeLeft;
-  }, [weddingDate]);
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [timeRemaining, setTimeRemaining] = useState(0);
 
   useEffect(() => {
-    setTimeLeft(calculateTimeLeft());
+    const countdownInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const eventTime = weddingDate.getTime();
+      const remainingTime = Math.max(eventTime - currentTime, 0);
 
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
+      setTimeRemaining(remainingTime);
+
+      if (remainingTime === 0) {
+        clearInterval(countdownInterval);
+        alert("It's party time!");
+      }
     }, 1000);
 
-    return () => clearInterval(timer);
-  }, [calculateTimeLeft]);
+    return () => clearInterval(countdownInterval);
+  }, [weddingDate]);
+
+  const countdown = (time: number) => {
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    return { days, hours, minutes, seconds };
+  };
+
+  const { days, hours, minutes, seconds } = countdown(timeRemaining);
 
   return (
     <Text>
-      {timeLeft.days} days, {timeLeft.hours} hours, {timeLeft.minutes} minutes,{" "}
-      {timeLeft.seconds} seconds
+      {days} days, {hours} hours, {minutes} minutes, {seconds} seconds
     </Text>
   );
-}
+};
+
+export default DaysToGo;
