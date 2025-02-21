e335b9548b5393fd91b3ac783c8f104669da216c
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-21 21:35:54 +0000

    black styling python

--- wedding-race-backend/main.py
+++ wedding-race-backend/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 app = FastAPI()
 
+
 @app.get("/api/weddingdate")
 async def get_wedding_date():
     wedding_date = datetime(2025, 4, 1, 13, 0, 0).strftime("%Y-%m-%dT%H:%M:%SZ")
