1114fd8eec4a2e7042ed9f3f284386786e6c9107
cjrace <52536248+cjrace@users.noreply.github.com>
2025-02-22 00:15:13 +0000

    update readme and check if new lint action fails

--- wedding-race-backend/main.py
+++ wedding-race-backend/main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 
 @app.get("/api/weddingdate")
 async def get_wedding_date():
-    wedding_date = datetime(2025, 4, 1, 13, 0, 0).strftime("%Y-%m-%dT%H:%M:%SZ")
+    wedding_date = datetime(2025,    4, 1, 13, 0, 0).strftime("%Y-%m-%dT%H:%M:%SZ")
     return {"wedding_date": wedding_date}
