0a71540bedcfa2399a5b9f2653cf6fa36f27229f
lauraselby <laura.selby@hotmail.co.uk>
2025-03-02 19:01:30 +0000

    faqs cintent

--- frontend/app/faqs/_faqsContent.tsx
+++ frontend/app/faqs/_faqsContent.tsx
@@ -1,9 +1,106 @@
-import { Text } from "@mantine/core";
+import { Text, Title, Space } from "@mantine/core";
 
 export default function FAQsContent() {
   return (
     <>
-      <Text>Some frequently asked questions about our wedding...</Text>
+
+      <Title order={2}>
+        Will there be a formal invite?
+      </Title>
+      <Text>
+         ???
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        How do I RSVP?
+      </Title>
+      <Text>
+        An RSVP section will be added to the website once accomodation booking opens (expected November 2025). There you&apos;ll be able to RSVP and share dietary requirements.
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        Can I still come to the pre-wedding festivities if not staying on site?
+      </Title>
+      <Text>
+        Yes, we would love to see you there! You&apos;ll just need to leave by 10pm as the venue request only guests staying on site remain after this time.
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        Can I bring my dog?
+      </Title>
+      <Text>
+        We are sorry but dogs aren&apos;t allowed in the wedding village.
+      </Text> 
+
+      <Space h="md" />
+      <Title order={2}>
+        I can&apos;t get there until the evening for the day before, is that okay?
+      </Title>
+      <Text>
+        Yes, of course! Join from whenever you&apos;re able after 4pm.
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        What time should I arrive for the wedding if not staying on site the night before?
+      </Title>
+      <Text>
+        Guests will be seated for the ceremony at 12:45, so please just make sure you&apos;re there before then. If you arrive early there&apos;ll be people around to point you in the right direction, and you will be able to wait with other guests in the wedding village. Note the bar won&apos;t be open until after the ceremony. 
+      </Text> 
+
+      <Space h="md" />
+      <Title order={2}>
+        Do I need to bring any food or drink with me for the evening before?
+      </Title>
+      <Text>
+        We will be providing food and drink so there&apos;s no need to bring anything, though if you would like to bring your own that&apos;s fine too! There are bottle fridges available at the village to store drinks.
+      </Text> 
+
+      <Space h="md" />
+      <Title order={2}>
+        Do I need to join in for everything or is it okay to chill in my pod?
+      </Title>
+      <Text>
+        We want you to feel comfortable and enjoy your stay so you can do as much or as little as you like. 
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        What should I wear?
+      </Title>
+      <Text>
+        Evening before is casual, then fancy it up for our wedding day.
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        What if I have a dietary restriction?
+      </Title>
+      <Text>
+        For the wedding day food we&apos;ll collect dietary requirements as part of your RSVP. We&apos;ll then use this info to make sure we have suitable food and drinks for anyone staying in the wedding village the night before.
+      </Text>
+
+      <Space h="md" />
+      <Title order={2}>
+        What is included in the accommodation?
+        </Title>
+      <Text>
+        The pods come with everything you need for a good nights sleep; a comfortable bed, bedding, towels, plugs for your gadgets and heating if you need it! There is self service tea and coffee in the Food Shack during your stay and breakfast is included.
+        </Text>
+      
+      <Space h="md" />
+      <Title order={2}>
+        Will there be somewhere for getting ready before the wedding?
+        </Title>
+      <Text>
+        The wedding village pods are spacious enough that getting ready shouldn&apos;t be a problem but for somethign more socialble we have a dedicated makeup shack for beautifying yourself with company. For bridesmiads we&apos;ll be getting ready in the cottages, which has a separate room for make up and hair. 
+        </Text>
+        
+      <Space h="md" />
+
     </>
   );
 }
