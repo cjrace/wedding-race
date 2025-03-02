ac887eba9770ebcd483b1ee693f6fa0f3e30100f
lauraselby <laura.selby@hotmail.co.uk>
2025-03-02 23:32:12 +0000

    homepage edits

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -3,12 +3,14 @@ import HomepageContent from "./_homepageContent";
 
 export default function HomePage() {
   return (
-    <>
+    <>    
       <Container style={{ width: "100%", padding: 0 }}>
         <Image
           radius="sm"
-          src="images/tipi-outside-arty-overlay.png"
+          src="images/tipi-outside-arty-overlay.svg"
           alt="Welcome to the Race Wedding"
+          width="100%"
+          fit="contain"
           style={{ width: "100%", height: "auto" }}
         />
       </Container>
