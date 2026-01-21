b114f01e86632327a9102f6de9f2aaf458d1d631
lauraselby <laura.selby@hotmail.co.uk>
2026-01-21 17:58:23 +0000

    anchor and location details

--- components/invitetext.tsx
+++ components/invitetext.tsx
@@ -2,8 +2,8 @@ import { Text } from "@mantine/core";
 
 export default function InviteText({ preWedding }: { preWedding: boolean }) {
   const inviteText = preWedding
-    ? "We're excited to officially invite you to our wedding! The celebration runs over three days from Monday 8th through to Wednesday 10th June, with pre-wedding partying at our on-site 'wedding village' the evening before and breakfast together the morning after. We'd love you to join us for the full three-day party if you can, but if not, no worries at all, just come for as much as you're able."
-    : "We are delighted to officially invite you to our wedding day on Tuesday 9th June 2026!";
+    ? "We're excited to officially invite you to our wedding at Lake Henry (DL7 0JW)! The celebration runs over three days from Monday 8th through to Wednesday 10th June, with pre-wedding partying at our on-site 'wedding village' the evening before and breakfast together the morning after. We'd love you to join us for the full three-day party if you can, but if not, no worries at all, just come for as much as you're able."
+    : "We are delighted to officially invite you to our wedding day at Lake Henry (DL7 0JW) on Tuesday 9th June 2026!";
 
   return <Text px={0}>{inviteText}</Text>;
 }
